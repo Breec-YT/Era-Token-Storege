052b4aab2be280e69b318a9fbbee98767fed663c
Breec <111704297+Breec-YT@users.noreply.github.com>
2023-04-03 04:00:15 +0200

    Fixed: The Console opening

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+import subprocess
 try:
     if os.path.isfile('Logintoken.txt'):
         with open('Logintoken.txt', 'r') as file:data = file.read()
         os.system('echo | set /p nul=' + data.strip() + '| clip')
     else:open('Logintoken.txt', 'a').close()
-    os.system('FortniteLauncher.exe')
+    subprocess.Popen("FortniteLauncher.exe")
 except:
     pass
